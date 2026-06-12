9c50b6697f920ed40e10934f1f69aacf535bd95e
alexismr1988 <alexismr7777@gmail.com>
2026-06-12 18:44:53 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/alexis/football_league_simulator_api/FootballLeagueSimulatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.alexis.football_league_simulator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLeagueSimulatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLeagueSimulatorApiApplication.class, args);
+	}
+
+}
